d4e3b3549e51937bcd29d02cc67584a418cf6a7a
Ang Li <ang.li@ericsson.com>
2015-10-26 13:24:54 +0800

    Update TODO

--- TODO.m
+++ TODO.m
@@ -2,3 +2,20 @@
 % buy in strategy
 % compound pct
 % ban swing
+
+% get all data from 2000.1.1
+% sync with baidu cloud
+
+
+% N/2 *4, N = 20%, M = 5, randomly choose
+
+
+% ref price: close, max and min
+
+% ma ref.: cross
+
+% single ma
+
+
+
+ 
